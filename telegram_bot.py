61c1de5d37af8adcd8fb0524fdcc2bec1a1d8446
Владислав Юров <clydeurov@gmail.com>
2022-08-24 10:50:32 +0300

    fixed image upload error

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,7 +9,7 @@ tg_chat_id = os.getenv("TG_CHAT_ID")
 bot = telegram.Bot(token=tg_token)
 print(bot.get_me())
 
-bot.send_document(chat_id=TG_CHAT_ID, document=open('images/nasa_apod_12', 'rb'))
+bot.send_document(chat_id=tg_chat_id, document=open('images/nasa_apod_12.jpg', 'rb'))
 
 # updates = bot.get_updates()
 # print([u.message.photo for u in updates if u.message.photo])
