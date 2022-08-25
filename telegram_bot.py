a03c751d1ee3648aab72f7558b0c8977d466ab23
Владислав Юров <clydeurov@gmail.com>
2022-08-25 16:34:00 +0300

    added the ability to make changes when running the code, added looping code in telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,44 @@
 import telegram
 import os
 from dotenv import load_dotenv
+import random
+import argparse
+import time
+import requests
 
-load_dotenv()
-tg_token = os.getenv("TG_TOKEN")
-tg_chat_id = os.getenv("TG_CHAT_ID")
 
-bot = telegram.Bot(token=tg_token)
-print(bot.get_me())
+def publication_photo(all_images):
+	for image in all_images:
+		bot.send_document(
+			chat_id=tg_chat_id, 
+			document=open(f'images/{image}', 'rb')
+		)
+		time.sleep(args.delay_time)
 
-bot.send_document(chat_id=tg_chat_id, document=open('images/nasa_apod_12.jpg', 'rb'))
 
-# updates = bot.get_updates()
-# print([u.message.photo for u in updates if u.message.photo])
+if __name__ == "__main__":
+	load_dotenv()
+	tg_token = os.getenv("TG_TOKEN")
+	tg_chat_id = os.getenv("TG_CHAT_ID")
+	bot = telegram.Bot(token=tg_token)
+	parser = argparse.ArgumentParser(
+		description="Размещает фото на телеграмм канал"
+	)
+	parser.add_argument(
+		'-d',
+		dest='delay_time',
+		help='Время задержки',
+		default=14400,
+		type=int
+	)
+	args = parser.parse_args()
+	
+	while True:
+		all_images = [image for image in os.walk("images")][0][2]
+		random.shuffle(all_images)
+		publication_photo(all_images)
+		exec(open("fetch_spacex_images.py").read())
+		exec(open("fetch_nasas_images.py").read())
+		exec(open("fetch_nasas_epic.py").read())
+	
+	
