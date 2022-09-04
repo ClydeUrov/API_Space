26bb8c76524b30ac6eb1187f48d3321866077fad
Владислав Юров <clydeurov@gmail.com>
2022-09-04 19:41:19 +0300

    repetitive image loading code moved to a separate file

---
+++ download_space_image.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def download_image(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == "__main__":
+    download_image(url, filename)
