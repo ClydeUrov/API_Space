a9dded3dcf88fc63290dba9a797fb032c0c5930f
Владислав Юров <clydeurov@gmail.com>
2022-09-12 11:04:38 +0300

    optimized code, added ability to redefine Token and chat id

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,39 +4,37 @@ from dotenv import load_dotenv
 import random
 import argparse
 import time
-import requests
 
- 
-def publication_photo(all_images):
+
+def publication_photo(all_images, bot, tg_chat_id):
 	for image in all_images:
-		with open(os.path.join('images', f'{image}'), 'rb') as file:
+		with open(os.path.join('images', image), 'rb') as file:
 			bot.send_document(
-				chat_id=os.environ.get("TG_CHAT_ID"),
+				chat_id=tg_chat_id,
 				document=file
 			)
-			file.close()
 		time.sleep(args.delay_time)
 
 
 def main():
+	redefine_tg_env = input("Желаете ли переопределить TG_TOKEN и TG_CHAT_ID? ")
+	if redefine_tg_env == "Yes" or redefine_tg_env == "Да":
+		os.environ['TG_TOKEN'] = str(input('Введите ваш TG_TOKEN: '))
+		os.environ['TG_CHAT_ID'] = str(input('Введите ваш TG_CHAT_ID: '))
+
+	bot = telegram.Bot(token=os.environ.get("TG_TOKEN"))
+	tg_chat_id = os.environ.get("TG_CHAT_ID")
 	all_images = [image for image in os.walk("images")][0][2]
 	while True:
 		try:
-			publication_photo(all_images)
+			publication_photo(all_images, bot, tg_chat_id)
 			random.shuffle(all_images)
 		except telegram.error.NetworkError:
 			time.sleep(10)
-			main()
-		except requests.ConnectionError:
-			time.sleep(10)
-			main()
 
 
 if __name__ == "__main__":
 	load_dotenv()
-	os.environ['TG_TOKEN'] = str(input('Введите ваш TG_TOKEN: '))
-	os.environ['TG_CHAT_ID'] = str(input('Введите ваш TG_CHAT_ID: '))
-	bot = telegram.Bot(token=os.environ.get("TG_TOKEN"))
 	parser = argparse.ArgumentParser(
 		description="Размещает фото на телеграмм канал"
 	)
