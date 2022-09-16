047b6202cfe67bb6326d6d0137dbf1fc07ab6a29
Владислав Юров <clydeurov@gmail.com>
2022-09-16 10:48:01 +0300

    added the ability to specify NASA Token in the start parameters, the code has been optimized

--- fetch_nasas_epic.py
+++ fetch_nasas_epic.py
@@ -1,25 +1,24 @@
 import os
 import requests
-from pathlib import Path
 from dotenv import load_dotenv
 from datetime import datetime
 import download_space_image
 import argparse
 
 
-def fetch_nasa_epic(token):
+def fetch_nasa_epic(token, photos_number):
     params = {'api_key': token}
     epic_response = requests.get(
-      'https://api.nasa.gov/EPIC/api/natural/images',
-      params=params
+        'https://api.nasa.gov/EPIC/api/natural/images',
+        params=params
     )
     epics_url = []
-    for number in range(0, photos_number()):
+    for number in range(0, photos_number):
         epic_name = epic_response.json()[number]['image']
         epic_time = datetime.fromisoformat(epic_response.json()[number]['date'])
         response = requests.get(
-          f'https://api.nasa.gov/EPIC/archive/natural/{epic_time.year}/{epic_time.strftime("%m")}/{epic_time.strftime("%d")}/png/{epic_name}.png',
-          params=params
+            f'https://api.nasa.gov/EPIC/archive/natural/{epic_time:%Y}/{epic_time:%m}/{epic_time:%d}/png/{epic_name}.png',
+            params=params
         )
         response.raise_for_status()
         epics_url.append(response.url)
@@ -34,13 +33,16 @@ if __name__ == '__main__':
     parser.add_argument(
         '-d',
         dest='photos_number',
-        help='Колличество скачиваемых фотографий NASA EPIC.',
+        help='Количество скачиваемых фотографий NASA EPIC.',
         default='5',
         type=int
     )
-    photos_number = lambda: parser.parse_args().photos_number
-    redefine_nasa_env = input("Желаете ли переопределить NASA_TOKEN? ")
-    if redefine_nasa_env == "Yes" or redefine_nasa_env == "Да":
-        os.environ['NASA_TOKEN'] = str(input('Введите ваш NASA_TOKEN: '))
-    Path("images").mkdir(parents=True, exist_ok=True)
-    fetch_nasa_epic(os.environ.get('NASA_TOKEN'))
+    parser.add_argument(
+        '-t',
+        dest='nasa_token',
+        help='NASA Токен',
+        default=os.environ['NASA_TOKEN']
+    )
+    get_photos_number = lambda: parser.parse_args().photos_number
+    get_nasa_token = lambda: parser.parse_args().nasa_token
+    fetch_nasa_epic(get_nasa_token(), get_photos_number())
