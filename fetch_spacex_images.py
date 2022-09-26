db3490976259d699e94229df7ed984e2e7311df1
Владислав Юров <clydeurov@gmail.com>
2022-09-26 19:42:13 +0300

    Made changes to the code formatting

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,7 +1,9 @@
-import os
-import requests
-import download_space_image
 import argparse
+import os
+
+import requests
+
+import download_space_image
 
 
 class EmptyListError(ValueError):
@@ -9,25 +11,20 @@ class EmptyListError(ValueError):
 
 
 def fetch_spacex_last_launch(launch):
-    response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch}')
+    response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch}")
     response.raise_for_status()
-    spacex_links = response.json()['links']['flickr']['original']
-    if not spacex_links:
-        raise EmptyListError('Отсутствуют картинки в данном запуске')
-    for spacex_number, spacex in enumerate(spacex_links):
-        filename = os.path.join('images', f'spacex_{spacex_number}.jpg')
+    links = response.json()["links"]["flickr"]["original"]
+    if not links:
+        raise EmptyListError("Отсутствуют картинки в данном запуске")
+    for number, spacex in enumerate(links):
+        filename = os.path.join("images", f"spacex_{number}.jpg")
         download_space_image.download_image(spacex, filename)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Выгружает картинки SpaceX"
-    )
+    parser = argparse.ArgumentParser(description="Выгружает картинки SpaceX")
     parser.add_argument(
-        '-d',
-        dest='launch_id',
-        help='ID запуска SpaceX',
-        default='latest'
+        "-d", dest="launch_id", help="ID запуска SpaceX", default="latest"
     )
     get_launch_id = lambda: parser.parse_args().launch_id
     fetch_spacex_last_launch(get_launch_id())
